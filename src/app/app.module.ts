0b505105661a0441e33c239b4c723f9e116fa85e
aswins2108@gmail.com <aswins2108@gmail.com>
2024-02-03 17:18:09 +0530

    Added the component structure with nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { RecipiesComponent } from './component/recipies/recipies.component';
+import { RecipieListComponent } from './component/recipies/recipie-list/recipie-list.component';
+import { RecipieItemComponent } from './component/recipies/recipie-list/recipie-item/recipie-item.component';
+import { ShoppingListComponent } from './component/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './component/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailsComponent } from './component/recipies/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/recipies/recipie-list/recipie-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipie-list',
+  templateUrl: './recipie-list.component.html',
+  styleUrl: './recipie-list.component.css'
+})
+export class RecipieListComponent {
+
+}
